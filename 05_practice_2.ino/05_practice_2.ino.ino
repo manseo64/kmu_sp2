cf024c1feb5034cfa82dc814e111f178a1ec76d4
manseo64 <cmseo3174938@naver.com>
2024-09-23 13:03:03 +0900

    05P12_modified ver

--- 05_practice_2.ino/05_practice_2.ino.ino
+++ 05_practice_2.ino/05_practice_2.ino.ino
@@ -1,15 +1,13 @@
 #define PIN_LED 7
 
 void setup() {
-  pinMode(PIN_LED, OUTPUT);   // 7번 핀을 출력 모드로 설정
+  pinMode(PIN_LED, OUTPUT);
 }
 
 void loop() {
-  // 1. 처음 1초 동안 LED를 켬
   digitalWrite(PIN_LED, LOW);
   delay(1000);
 
-  // 2. 그 다음 1초 동안 5회 깜빡임 (0.2초 간격)
   for (int i = 0; i < 5; i++) {
     digitalWrite(PIN_LED, LOW);
     delay(100);
@@ -17,7 +15,6 @@ void loop() {
     delay(100);
   }
 
-  // 3. LED 끄고 무한 루프 
   digitalWrite(PIN_LED, HIGH);
   while (1) {
     
